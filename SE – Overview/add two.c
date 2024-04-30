f54ef3601c2bf8493601fb9586ac16da503df232
yeshamodi12 <148933660+yeshamodi12@users.noreply.github.com>
2024-04-30 10:52:47 +0530

    done

---
+++ SE – Overview/add two.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+//	int  : containing whole value %d
+//	float : containing decimal %f
+//	char : containing text %c or %s
+float pie=3.14;
+float r,answer;
+
+printf("answer is:%.1f",ans);
+
+}
+
+printf()
+scanf()
